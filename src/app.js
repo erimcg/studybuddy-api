b8b631a74df092e03321cff09cfba672cb0ceca3
Eric McGregor <eric@n0code.net>
2024-01-29 00:32:55 -0500

    added default handler

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 const express = require('express')
+
+// npm install cors --save
+const cors = require('cors');
+
 const userRouter = require('./routers/user')
 
 const app = express()
 
-// npm install cors --save
-const cors = require('cors');
+
 app.use(cors())
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -14,6 +17,10 @@ app.use(function (req, res, next) {
 
 app.use(express.json())
 app.use(userRouter)
+
+app.get('', (req, res) => {
+    res.send("hello")
+})
 
 const port = process.env.PORT || 3000
 
