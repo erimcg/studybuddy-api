6a03776be89d13bf357918ef11d466e6086deb2e
Eric McGregor <eric@n0code.net>
2024-01-29 03:06:19 -0500

    changed endpoints

--- src/routers/user.js
+++ src/routers/user.js
@@ -5,8 +5,8 @@ const router = express.Router()
 router.post('/user', async (req, res) => {
     console.log(req.body)
     let name = req.body.name
-    //res.status(201).send(req.body)
-    res.send("post /user" + name)
+    res.status(201).send(req.body)
+    //res.send("post /user" + name)
 })
 
 router.get('/user', (req, res) => {
