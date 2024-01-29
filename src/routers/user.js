eb7ab62cf1cddf630f70869e9ca06b42806ce2d9
Eric McGregor <eric@n0code.net>
2024-01-29 01:03:58 -0500

    added stderr message in user router

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,10 +1,11 @@
 const express = require('express')
 
 const router = express.Router()
+process.stdout.write("setting up user router\n")
 
 router.post('/user', (req, res) => {
     console.log(req.body)
-
+    process.stdout.write('processing post /user\n')
     res.status(201).send(req.body)
 })
 
