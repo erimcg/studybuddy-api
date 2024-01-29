076ecb15ce9b0c40e5503a617988a6331b3254b0
Eric McGregor <eric@n0code.net>
2024-01-28 23:56:11 -0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const userRouter = require('./routers/user')
+
+const app = express()
+
+// npm install cors --save
+const cors = require('cors');
+app.use(cors())
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.use(express.json())
+app.use(userRouter)
+
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
---
+++ src/routers/user.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.post('/user', (req, res) => {
+    console.log(req.body)
+
+    res.status(201).send(req.body)
+})
+
+module.exports = router
