efba559e063e9dffe5e01950eeddd11eb11384c1
Eric McGregor <eric@n0code.net>
2024-01-31 00:15:32 -0500

    added log to post /user handler

--- src/routers/user.js
+++ src/routers/user.js
@@ -5,12 +5,13 @@ const router = express.Router()
 
 router.post('/user', async (req, res) => {
     const user = new User(req.body)
+    console.log(user)
     
     try {
         await user.save()
         res.status(201).send(user)
     } catch (error) {
-        res.status(400).send(error)
+        res.status(400).send(user)
     }
 })
 
