6a39c2b656be2e04fd6fda34b5f422f7b22bbe8d
Eric McGregor <eric@n0code.net>
2024-02-01 18:32:22 -0500

    added email, password hash, toJSON

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcrypt')
+
 const Schema = mongoose.Schema
 const userSchema = new Schema({
-    email: { type: String, unique: true, required: true, trim: true, lowercase: true },
+    email: {
+        type: String, unique: true, required: true, trim: true, lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) { throw new Error('Email is invalid.') }
+        }
+    },
     username: { type: String, required: true, trim: true },
     password: { type: String, required: true, trim: true, minLength: 8 },
     school: { type: String, required: true },
@@ -9,6 +17,24 @@ const userSchema = new Schema({
     majors: [String], tokens: [String],
     profile_pic: Buffer
 })
+
+userSchema.pre('save', async function (next) {
+    const user = this
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+    next() // continue and run the save() method 
+})
+
+userSchema.methods.toJSON = function () {
+    const user = this
+    const userObject = user.toObject()
+    
+    delete userObject.password
+    delete userObject.__v
+    
+    return userObject
+}
 
 const User = mongoose.model('User', userSchema);
 module.exports = User
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const User = require('../models/user')
+const mongoose = require('mongoose')
+const { sendVerificationEmail } = require('../emails/account.js')
 
 const router = express.Router()
 
 router.post('/user', async (req, res) => {
     const user = new User(req.body)
-    console.log(user)
     
     try {
-        await user.save()
+        //await user.save()
+        sendVerificationEmail(user.email, user.username)
         res.status(201).send(user)
     } catch (error) {
         res.status(400).send(error)
