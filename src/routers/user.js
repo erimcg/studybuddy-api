45b84ce6258e2cc50a4a17970b0df7310fed5ae2
Eric McGregor <eric@n0code.net>
2024-02-04 11:26:27 -0500

    added basic user/verification endpoint

--- src/routers/user.js
+++ src/routers/user.js
@@ -6,15 +6,23 @@ const { sendVerificationEmail } = require('../emails/account.js')
 const router = express.Router()
 
 router.post('/user', async (req, res) => {
-    const user = new User(req.body)
     
+    delete req.body.email_verified
+    delete req.body.tokens
+    
+    const user = new User(req.body)
+
     try {
-        //await user.save()
+        await user.save()
         sendVerificationEmail(user.email, user.username)
         res.status(201).send(user)
     } catch (error) {
         res.status(400).send(error)
     }
+})
+
+router.get('/user/verification', async (req, res) => {
+    res.send()
 })
 
 module.exports = router
